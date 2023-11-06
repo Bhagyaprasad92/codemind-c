82dd74013eb642f52341cd409cb02db141e6cc76
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2023-11-06 15:08:33 +0530

    #include<stdio.h> int main(){     int a,b,c;     scanf("%d",&a);         b=a%10;         while(a>=10){         a/=10;}         a==a;      printf("%d",b+a); }

--- Sum_of__First_and_Last_digits_.c
+++ Sum_of__First_and_Last_digits_.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a,b;
+    int a,b,c;
     scanf("%d",&a);
-    b=a%10;
-    while(a>=10){
-        a/=10;
-    }
-    a==a;
-    printf("%d",a+b);
+        b=a%10;
+        while(a>=10){
+        a/=10;}
+        a==a;
+     printf("%d",b+a);
 }
