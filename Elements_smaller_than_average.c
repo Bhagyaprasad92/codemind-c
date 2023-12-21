deae582c9132b51d9a6586f9966c9b3d9502dbd5
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2023-12-21 21:59:11 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int ar[a],sum=0;     for(int i=0;i<a;i++){         scanf("%d",&ar[i]);         sum+=ar[i];     }     int avg=sum/a,c=0;     for(int i=0;i<a;i++){         if(ar[i]<=avg){             c++;         }     }     printf("%d",c); }

--- Elements_smaller_than_average.c
+++ Elements_smaller_than_average.c
@@ -2,14 +2,12 @@
 int main(){
     int a;
     scanf("%d",&a);
-    int ar[a];
+    int ar[a],sum=0;
     for(int i=0;i<a;i++){
         scanf("%d",&ar[i]);
+        sum+=ar[i];
     }
-    int avg,sum=0,c=0;
-     for(int i=0;i<a;i++){
-        sum+=ar[i];}
-        avg=sum/a;
+    int avg=sum/a,c=0;
     for(int i=0;i<a;i++){
         if(ar[i]<=avg){
             c++;
